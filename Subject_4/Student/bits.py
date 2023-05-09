c4b2fda5c44578a63853ae786ba121bab00d62d5
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2023-05-09 17:25:53 +0200

    feature: add subject, need to do enigma student subject

---
+++ Subject_4/Student/bits.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+#
+# EPITECH PROJECT, 2023
+# WelcomeDayPromo2028
+# File description:
+# bits.py
+#
+
+binary_string = "00010000 00000001 00010100 00001000 00010100 00000101 00001011"
+
+# Sépare les groupes de chiffres binaires :
+# binary_list = ____
+
+numbers = []
+# Convertit chaque groupe de chiffres binaires en nombre décimal :
+
+print(numbers)
+
+# numbers = [____]
+
+# alphabet = "____"
+
+letters = []
+
+# Convertit chaque nombre en lettre de l'alphabet :
+
+word = ""
+print(word)
