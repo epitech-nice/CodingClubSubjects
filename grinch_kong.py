77dc9d37ccfe236ad33bd977b62a59124a0d8676
AlexandreDFM <alexandre-kevin.de-freitas-martins@epitech.eu>
2024-01-02 03:30:26 +0100

    refactor(code): clean the code

--- grinch_kong.py
+++ grinch_kong.py
@@ -17,7 +17,6 @@ class Game:
     def __init__(self):
         # Initialize Pygame
         pygame.init()
-
         self.inLoop, self.inMenu, self.inGame = True, True, False
 
         self.difficulty = 0
@@ -41,13 +40,13 @@ class Game:
         self.marioX, self.marioY = 150, 720
         self.jumpCount, self.jumpPoint = 0, 0
 
-        self.giftX, self.giftY, self.giftDirection = [], [], []
         self.throwCountdown = 0
         self.fall, self.fallCount = [], []
         self.giftLeft, self.giftRight = [], []
+        self.giftX, self.giftY, self.giftDirection = [], [], []
 
-        self.platInclineX = [100, 140, 190, 240, 280, 330, 380, 430, 480, 530, 570, 620, 670, 720]
         self.inclineCount = 0
+        self.platInclineX = [100, 140, 190, 240, 280, 330, 380, 430, 480, 530, 570, 620, 670, 720]
 
         self.ladderX1 = [295, 605, 295, 345, 345, 150, 245, 385, 600, 600, 245, 150, 265, 265, 315, 555, 555, 600, 440, 320]
         self.ladderX2 = [305, 610, 310, 350, 350, 160, 255, 400, 610, 610, 255, 160, 280, 280, 325, 565, 565, 610, 450, 335]
@@ -66,33 +65,33 @@ class Game:
         self.title = pygame.image.load("./assets/images/screen/title-screen.png")
         self.level = pygame.image.load("./assets/images/platform/level.png")
 
-        self.marioLeft = pygame.image.load("./assets/images/mario/mario-left.png")
-        self.marioRight = pygame.image.load("./assets/images/mario/mario-right.png")
         self.runLeft = pygame.image.load("./assets/images/mario/run-left.png")
         self.runRight = pygame.image.load("./assets/images/mario/run-right.png")
-        self.marioJumpLeft = pygame.image.load("./assets/images/mario/jump-left.png")
-        self.marioJumpRight = pygame.image.load("./assets/images/mario/jump-right.png")
+        self.marioLeft = pygame.image.load("./assets/images/mario/mario-left.png")
+        self.marioRight = pygame.image.load("./assets/images/mario/mario-right.png")
         self.marioClimb1 = pygame.image.load("./assets/images/mario/marioClimb1.png")
         self.marioClimb2 = pygame.image.load("./assets/images/mario/marioClimb2.png")
+        self.marioJumpLeft = pygame.image.load("./assets/images/mario/jump-left.png")
+        self.marioJumpRight = pygame.image.load("./assets/images/mario/jump-right.png")
         self.marioImage = self.marioRight
 
         self.santaHelp = pygame.image.load("./assets/images/santa/santa-help.png")
 
-        self.grinchForward = pygame.image.load("./assets/images/grinch/grinchForward.png")
         self.grinchLeft = pygame.image.load("./assets/images/grinch/grinchLeft.png")
         self.grinchRight = pygame.image.load("./assets/images/grinch/grinchRight.png")
+        self.grinchForward = pygame.image.load("./assets/images/grinch/grinchForward.png")
         self.grinchImage = self.grinchForward
 
-        self.giftStack = pygame.image.load("./assets/images/gift/gift-stack.png")
         self.giftDown = pygame.image.load("./assets/images/gift/gift-down.png")
+        self.giftStack = pygame.image.load("./assets/images/gift/gift-stack.png")
 
         self.giftSequence = load_images("./assets/images/gift", [f"gift{i}" for i in range(1, 5)])
         self.giftPic = []
 
-        self.walk = pygame.mixer.Sound("./assets/sounds/walking.wav")
         self.jump = pygame.mixer.Sound("./assets/sounds/jump.wav")
         self.intro = pygame.mixer.Sound("./assets/sounds/intro.wav")
         self.death = pygame.mixer.Sound("./assets/sounds/death.wav")
+        self.walk = pygame.mixer.Sound("./assets/sounds/walking.wav")
         self.bac = pygame.mixer.music.load("./assets/sounds/background_music.wav")
 
         self.screen = pygame.display.set_mode((800, 800))
@@ -141,7 +140,7 @@ class Game:
         @param: none
         @return: hit(boolean)
         """
-        for i in range(0, len(self.giftX)): #goes through all the gifts
+        for i in range(0, len(self.giftX)):
             if self.marioX + 20 >= self.giftX[i] and self.marioX <= self.giftX[i] + 26 and self.marioY + 30 >= self.giftY[i] and self.marioY <= self.giftY[i] + 20:
                 self.hit = True
         return self.hit
@@ -155,7 +154,7 @@ class Game:
         """
         global marioY
         upLadder, downLadder, moveSides  = False, False, True
-        for i in range(0, len(self.ladderX1)): #goes through all the ladders
+        for i in range(0, len(self.ladderX1)):
             if self.marioX >= self.ladderX1[i] and self.marioX <= self.ladderX2[i] and self.marioY >= self.ladderY1[i] and self.marioY <= self.ladderY2[i]: # if mario is in range of a ladder, he can move up, down, and to the sides
                 downLadder = True
                 upLadder = True
@@ -179,7 +178,6 @@ class Game:
         @param: y(int), x(int), direction(str), objectt(str)
         @return: y(int) or move(int)
         """
-        global inclineCount
         #lines 344 to 371 checks which platform the object is on and then declares the range where the object inclines and how much it moves vertically when going right on a inclined part
         if y <= 720 and y >= 657: #if the object is on the bottom platform
             startNum, endNum, move = 6, len(self.platInclineX) - 1, 3
@@ -225,9 +223,7 @@ class Game:
 
     def jump_manager(self):
         if self.jumpLeft or self.jumpRight or self.jumpStill: #if mario is jumping, change x and/or y values accordingly
-            #keeps track of how many jumps
             self.jumpCount = self.jumpCount + 1
-            #changes y coordinates
             self.marioY = self.marioY + self.addJump
             if self.jumpCount == 7: #when jumpCount is 7, make mario come back down by change the number he goes up/down by
                 self.addJump = 7
@@ -244,7 +240,6 @@ class Game:
                 self.inclineCount = 0
                 self.jumpLeft, self.jumpRight, self.jumpStill = False, False, False
             if self.marioX != 60 and self.marioX != 710 and (self.marioX != 320 or self.marioY >= 232): #if mario has reached a boundary on the sides, don't add to x values
-                #checks how many inclines mario jumped over and whether to move up or down when mario lands
                 self.move = self.incline(self.marioY, self.marioX, self.direction, "mario")
                 if self.jumpLeft and self.moveLeft: #if mario is jumping left and he can move left, minus 5 to his x coordinates
                     self.marioX = self.marioX - 5
@@ -255,7 +250,7 @@ class Game:
                     self.jumpPoint = 1
 
     def gift_manager(self):
-        for i in range(0, len(self.giftPic)): #goes through all the gifts
+        for i in range(0, len(self.giftPic)):
             if self.giftX[i] <= 31: #if the gift reaches the end of the structure, make the gift disappear off the screen
                 self.giftX[i], self.giftY[i] = -30, -30
             if self.fall[i] == False: #if the gift is not falling, check to see if it is
@@ -272,18 +267,14 @@ class Game:
                         self.giftX[i] = self.giftX[i] + 10
                     else:
                         self.giftX[i] = self.giftX[i] - 10
-                    #checks if the gift needs to incline up/down and changes the value in the function
                     self.giftY[i] = self.incline(self.giftY[i] - 11, self.giftX[i], self.giftDirection[i], "gift")
                     self.giftY[i] = self.giftY[i] + 11
-                    #subtracted 11 then added it back so that in the function, the values that the functions checks with the y value can be used for both the gift and mario
                 else: #else the gift is in the process of falling
-                    #add one to keep track of how long it has fallen
                     self.fallCount[i] = self.fallCount[i] + 1
                     if self.giftLeft[i] == False: #if the gift is falling on the left side, x is being subtracted by 5
                         self.giftX[i] = self.giftX[i] - 5
                     elif self.giftRight[i] == False: #if it's falling from the right x is being added by 5
                         self.giftX[i] = self.giftX[i] + 5
-                    #changing y by 7 each time
                     self.giftY[i] = self.giftY[i] + 7
                     if self.fallCount[i] == 8: #if the count has reached 8, stop falling and reset the values for the next time
                         #adjust to make sure it lands on platform right
@@ -292,7 +283,6 @@ class Game:
                         self.fallCount[i] = 0
                         self.fall[i] = False
                         self.giftLeft[i], self.giftRight[i] = True, True
-                #changes the picture of the gift each time
                 if self.giftPic[i] == self.giftSequence[3]: #if the giftPic is at index 3, change it to at index 0
                     self.giftPic[i] = self.giftSequence[0]
                 else: #else change it to the next number in the list
@@ -306,20 +296,16 @@ class Game:
                     self.giftChoice = random.randint(0, 1)
                     if self.giftChoice == 0: #if the random number that was picked is 0, the gift image and coordinates will be reset
                         self.giftPic[i] = self.giftDown
-                        #adjust a bit because the gift going down is wider than the other gift images
                         self.giftX[i] = self.giftX[i] - 2
                 if self.giftX[i] + 2 == self.giftLadderX[j] and self.giftY[i] == self.giftLadderY2[j]: #if the gift has reached the end of a ladder, reset the variables back
                     self.giftPic[i] = self.giftSequence[0]
                     self.giftX[i] = self.giftX[i] + 2
-                    #this makes sure that when it comes down it lands properly on the platform instead of 5 pixels too high, as the gifts move 10 pixels at a time
                     self.giftY[i] = self.giftY[i] + self.giftAdjust[j]
 
     def game_logic(self):
         self.hit = self.collide()
-        #checks if mario has hit a boundary to the left or right of him
         self.moveLeft, self.moveRight = self.boundaries(self.marioX, self.marioY)
         if self.hit == False: #if hit is fallse, mario has not hit a gift and normal game play continues
-            #checks if mario is on a ladder and whether he can go up, down or left and right
             self.upLadder, self.downLadder, self.moveSides = self.ladderCheck()
             if self.marioY <= 154: #if mario reaches a y value of less than or equal to 154, he has won the game
                 self.grinchClimb = -15
@@ -328,7 +314,6 @@ class Game:
             self.jump_manager()
             self.gift_manager()
             if self.throwGift == False: #if throwGift is false, get a random number to decide whether or not GRINCH will throw another gift
-                #after each level the range will be smaller, meaning a higher chance of throwing gifts
                 self.grinchChoice = random.randint(0, 50 - self.difficulty)
                 if self.grinchChoice == 0: #if the number is 0, reset variables to throw the gift
                     self.grinchImage = self.grinchLeft
@@ -337,7 +322,6 @@ class Game:
                     self.grinchImage = self.grinchForward
                     self.throwGift = False
             if self.throwGift: #if throwGift is true, go through these changes
-                #add to give GRINCH some time to get gift
                 self.throwCountdown = self.throwCountdown + 1
                 if self.throwCountdown == 20: #if throwCountdown is 20, create a new gift
                     self.grinchImage = self.grinchRight
@@ -363,7 +347,6 @@ class Game:
         if self.inGame and self.jumpLeft == False and self.jumpRight == False and self.jumpStill == False and self.hit == False:
             keys = pygame.key.get_pressed()
             if keys[pygame.K_LEFT] and self.moveSides and (self.marioX != 320 or self.marioY > 232) and self.moveLeft and self.marioX != 60: #looks for left arrow to be pressed
-                #changes mario's y to incline up/go down with the slope
                 self.marioY = self.incline(self.marioY, self.marioX, self.direction, "mario")
                 if self.direction == "left":
                     self.marioX = self.marioX - 5
@@ -376,12 +359,11 @@ class Game:
                 if keys[pygame.K_SPACE]: #if space is pressed while left is also being pressed, jumpLeft is True and change the image
                     self.jumpLeft = True
                     self.marioImage = self.marioJumpLeft
-                    # if not pygame.mixer.get_busy():
-                    pygame.mixer.Sound.stop(self.walk)
-                    pygame.mixer.Sound.play(self.jump)
+                    if not pygame.mixer.get_busy():
+                        pygame.mixer.Sound.stop(self.walk)
+                        pygame.mixer.Sound.play(self.jump)
                 self.direction = "left"
             elif keys[pygame.K_RIGHT] and self.moveSides and self.moveRight and self.marioX != 710: #looks for right arrow to be pressed
-                #changes mario's y to incline up/go down with the slope
                 self.marioY = self.incline(self.marioY, self.marioX, self.direction, "mario")
                 if self.direction == "right":
                     self.marioX = self.marioX + 5
@@ -436,14 +418,11 @@ class Game:
                         self.inGame = True
                         self.intro.stop()
                         pygame.mixer.music.play(-1)
-
             # Game Mouvements
             self.mouvements()
             self.clock.tick(30)
-
             # Update game state
             self.update()
-
             # Draw everything
             self.draw()
 
